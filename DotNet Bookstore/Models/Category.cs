f168f3207c569d898cd44bfabfbd66143e3cc8b2
Divine Mbamara <divinembamara@gmail.com>
2025-02-06 23:31:26 -0500

    Week 3 - P2 - Scaffold Create Form

--- DotNet Bookstore/Models/Category.cs
+++ DotNet Bookstore/Models/Category.cs
@@ -1,8 +1,14 @@
-﻿namespace DotNet_Bookstore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_Bookstore.Models
 {
     public class Category
     {
+        //pk should always be named Id or <classname>Id e.g. CategoryId
+        [Display(Name = "Category ID")]
+        [Range(1, 9999999)]
         public int CategoryId { get; set; }
+        [Required(ErrorMessage = "A customized error message")]
         public string Name { get; set; }
     }
 }
